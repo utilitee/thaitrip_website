0275be8cde7f422e08caccc92b92a9d30d6dc9f6
Moojee <sutitamoojee@gmail.com>
2024-12-15 14:58:18 +0700

    edit Button ScrollToTop
    Chek Scroll button

--- src/components/Button/ScrollToTopButton.jsx
+++ src/components/Button/ScrollToTopButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ScrollToTopButton = () => {
   const [showButton, setShowButton] = useState(false);
@@ -12,35 +12,41 @@ const ScrollToTopButton = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', checkScrollPosition);
+    window.addEventListener("scroll", checkScrollPosition);
     return () => {
-      window.removeEventListener('scroll', checkScrollPosition);
+      window.removeEventListener("scroll", checkScrollPosition);
     };
   }, []);
 
   const scrollToTop = () => {
+    console.log("Scrolling to top");
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
 
   return (
     showButton && (
-<button className="btn btn-circle bg-gray-800 text-white fixed bottom-4 right-4">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor">
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M19 9l-7-7-7 7" />
-  </svg>
-</button>
+      <button
+        className="btn btn-circle bg-gray-800 text-white fixed bottom-4 right-4"
+        onClick={scrollToTop}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M19 9l-7-7-7 7"
+          />
+        </svg>
+      </button>
     )
   );
 };
